969f6c175fd71c4500c7d3cac448d490384854ba
Azuan <alienxp03@gmail.com>
2016-05-13 21:59:39 +0800

    Initial Commit

---
+++ Meetapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meetapp
+//
+//  Created by Azuan on 13/05/2016.
+//  Copyright © 2016 Azuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
